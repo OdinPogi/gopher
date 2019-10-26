128d9064650eff076766dabf17d89edad6e02ed2
OdinPogi <dfp.marcelo@gmail.com>
2019-10-25 23:15:50 -0400

    Add package a

---
+++ modules/submodules/a/a.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/OdinPogi/gopher/modules/submodules/b"
+)
+
+const Name = b.Name
+
+func main() {
+	fmt.Println(Name)
+}
---
+++ modules/submodules/a/go.mod
@@ -0,0 +1,5 @@
+module github.com/OdinPogi/gopher/modules/submodules/a
+
+go 1.13
+
+require github.com/OdinPogi/gopher/modules/submodules/b v0.1.1
